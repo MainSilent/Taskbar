74a585e410ec036ed4f789e3ae33e7071f38cdc2
Hamidreza Baniasadi <hamidreza.neo@gmail.com>
2020-11-16 13:05:21 +0330

    .

---
+++ Taskbar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Taskbar
+{
+    public class Main
+    {
+        public static dynamic callback;
+        // Get all opened programs
+        public async Task<object> init(dynamic input)
+        {
+            callback = input;
+            
+            //((Func<object, Task<object>>)Main.callback)("fgh");
+            new MainWindow();
+
+            return null;
+        }
+    }
+}
